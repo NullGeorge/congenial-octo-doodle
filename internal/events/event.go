192bcafd010de1a2177e88c3a00897a9ae7d4d76
George <24947331+NullGeorge@users.noreply.github.com>
2026-08-15 14:02:48 +0300

    add event model

---
+++ internal/events/event.go
@@ -0,0 +1,26 @@
+package events
+
+import "time"
+
+type Type string
+
+const (
+	KnockReceived       Type = "knock.received"
+	SequenceStarted     Type = "knock.sequence_started"
+	SequenceMatched     Type = "knock.sequence_matched"
+	SequenceFailed      Type = "knock.sequence_failed"
+	AccessGranted       Type = "access.granted"
+	AccessRevoked       Type = "access.revoked"
+	KnockdStarted       Type = "knockd.started"
+	KnockdStopped       Type = "knockd.stopped"
+)
+
+type Event struct {
+	ID        string    `json:"id"`
+	Type      Type      `json:"type"`
+	Timestamp time.Time `json:"timestamp"`
+	SourceIP  string    `json:"source_ip,omitempty"`
+	Rule      string    `json:"rule,omitempty"`
+	Port      uint16    `json:"port,omitempty"`
+	Message   string    `json:"message,omitempty"`
+}
